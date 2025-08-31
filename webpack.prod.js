b95f51d9fa6f670d32babd7747fbf7230985ab74
andiryanto <mandiryanto28@gmail.com>
2025-08-31 23:33:13 +0700

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,29 @@
 /* eslint-disable import/no-extraneous-dependencies */
-
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'public'), // hasil build masuk ke /public
+    filename: 'bundle.js',
+    clean: true, // hapus isi folder public tiap build biar fresh
+  },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: '/node_modules/',
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-        },
-      }],
-    }],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
   },
 });
