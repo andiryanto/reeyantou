405d9eb82ec70e76c3f6a3cd9a3a6ef980570724
andiryanto <mandiryanto28@gmail.com>
2025-08-31 23:39:50 +0700

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -14,7 +14,7 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.js$/,
-        exclude: '/node_modules/',
+        exclude: /node_modules/,
         use: [
           {
             loader: 'babel-loader',
